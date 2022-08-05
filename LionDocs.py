d07764708afdee5f11ce0a6c8e0724e6d362cbf3
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2022-08-05 10:55:29 -0400

    Some improvements
    * Code refactoring
    * Added function to copy SHA to clipboard
    * Added file transfer function with sourceCommit

--- LionDocs.py
+++ LionDocs.py
@@ -1,4 +1,5 @@
 import os
+import re
 from pathlib import Path
 
 import sublime
@@ -12,20 +13,25 @@ try:
 except Exception:
     from api import Shaman
 
-CONTENT = None
-TRANSLATED_CONTENT = None
+CONTENT_PATH = None
+TRANSLATED_CONTENT_PATH = None
 LANG_CODE = None
+
+cpath = str(Path("\\content\\"))
+tcpath = str(Path("\\translated-content\\"))
+uspath = str(Path("\\en-us\\"))
+
 valid_exts = ('.md', '.html')
 exts_dict = {'.md': '.html', '.html': '.md'}
 
 
 def plugin_loaded():
-    global CONTENT
-    global TRANSLATED_CONTENT
+    global CONTENT_PATH
+    global TRANSLATED_CONTENT_PATH
     global LANG_CODE
     settings = sublime.load_settings('LionDocs.sublime-settings')
-    CONTENT = settings.get('paths').get('content')
-    TRANSLATED_CONTENT = settings.get('paths').get('translated-content')
+    CONTENT_PATH = settings.get('paths').get('content')
+    TRANSLATED_CONTENT_PATH = settings.get('paths').get('translated-content')
     LANG_CODE = settings.get('lang_code')
 
 
@@ -42,50 +48,68 @@ class getshaCommand(sublime_plugin.TextCommand):
         file_ext = target_file.suffix
 
         if file_ext in valid_exts:
-            tmp = str(target_file).replace("translated-content", "content")
-            lang = "\\" + LANG_CODE + "\\"
-            target_in_content = Path(tmp.replace(lang, "\\en-us\\"))
+            # replace translated-content with content
+            tmp = str(target_file).replace(tcpath, cpath)
+
+            # replace en-us with target language
+            lang = str(Path("\\" + LANG_CODE))
+            target_in_content = Path(tmp.replace(lang, uspath))
 
             meta = None
 
             if target_in_content.is_file():
-                shaman = Shaman(target_in_content, CONTENT)  # here?
+                shaman = Shaman(target_in_content, CONTENT_PATH)  # here?
                 meta = shaman.get_file_sha(returnas='meta')
             else:
+                # try switching extension for find target file
                 switch_ext = target_in_content.with_suffix(exts_dict[file_ext])
                 target_in_content = switch_ext
 
                 if target_in_content.is_file():
-                    shaman = Shaman(target_in_content, CONTENT)
+                    shaman = Shaman(target_in_content, CONTENT_PATH)
                     meta = shaman.get_file_sha(returnas='meta')
                 else:
                     # ??? File doesn't exist in content? Update content repo
-                    raise Exception('File does not exist in content?')
+                    # raise Exception('File does not exist in content?')
+                    sublime.message_dialog("File does not exist in content?"
+                                           " Please sync your forks")
 
             if mode == 'insert':
                 self.__insert_in_cursor(edit, meta)
             elif mode == 'clipboard':
-                sublime.message_dialog("TODO :)")
+                sublime.set_clipboard(meta)
 
 
 class transferCommand(sublime_plugin.TextCommand):
     def run(self, edit, mode):
-        target_file = Path(self.view.file_name())
+        file_to_transfer = Path(self.view.file_name())  # file in content
 
-        rpl1 = str(target_file).replace("\\content\\", "\\translated-content\\")
-        rpl2 = str(rpl1).replace("\\en-us\\", "\\" + LANG_CODE + "\\")
-        # print(LANG_CODE)
-        dir_tree = str(Path(rpl2).parent)
+        # replace content with translated-content
+        temp = str(file_to_transfer).replace(cpath, tcpath)
 
+        # replace en-us with target language
+        lang = str(Path("\\" + LANG_CODE))
+        final_file_path = temp.replace(uspath, lang)
+
+        # get final file dir tree
+        dir_tree = str(Path(final_file_path).parent)
+
+        # create dir tree if not exist
         if not os.path.exists(dir_tree):
             os.mkdir(dir_tree)
 
-        if mode == 'same_file':
-            with open(str(target_file), 'r') as to_read, open(rpl2, 'w') as to_write:
-                content = to_read.read()
-                to_write.write(content)
+        # open original and destiny file at same time
+        with open(str(file_to_transfer), 'r') as original_file, open(final_file_path, 'w') as final_file:
+            original_content = original_file.read()
 
-            sublime.message_dialog("File transfered successfully!")
+            if mode == 'same_file':
+                final_file.write(original_content)
+                sublime.message_dialog("File transfered successfully!")
 
-        elif mode == 'with_sha':
-            sublime.message_dialog("TODO :)")
+            elif mode == 'with_sha':
+                shaman = Shaman(file_to_transfer, CONTENT_PATH)
+                meta = shaman.get_file_sha(returnas='meta')
+
+                sep_index = [i.start() for i in re.finditer('---', original_content)][1]  # get separator index
+                final_content = original_content[:sep_index] + meta + "\n" + original_content[sep_index:]
+                final_file.write(final_content)
