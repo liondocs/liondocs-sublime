d18640b1a8435ec97fd0131bcc0fe1aa56b9e13f
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2022-10-23 00:35:42 -0400

    fix(core): usage of an unexisting variable

--- LionDocs.py
+++ LionDocs.py
@@ -91,7 +91,7 @@ def validateConfig(func):
             func(self, edit, mode)
         else:
             log("[CONFIG  ] Configuration parameters are not valid, please check")
-            alert("Please check LionDocs configuration, you have {0} empty values".format(err_count))
+            alert("Please check LionDocs configuration, you have empty parameters")
 
     return wrapper
 
